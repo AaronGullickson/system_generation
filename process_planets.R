bfe12589fc510617ac04af971ab141a310c559b7
Aaron Gullickson <aarong@uoregon.edu>
2018-09-25 07:01:41 -0700

    initial commit

---
+++ process_planets.R
@@ -0,0 +1,101 @@
+library(xml2)
+planets <- read_xml("planets.xml")
+
+planet.table <- NULL
+target.year <- 3067
+
+for(i in 1:xml_length(planets)) {
+  planet <- xml_children(planets)[[i]]
+  pname <- xml_text(xml_find_first(planet, "id"))
+  xcood <- as.numeric(xml_text(xml_find_first(planet, "xcood")))
+  ycood <- as.numeric(xml_text(xml_find_first(planet, "ycood")))
+  faction <- xml_text(xml_find_first(planet, "faction"))
+  hpg <- xml_text(xml_find_first(planet, "hpg"))
+  sic <- xml_text(xml_find_first(planet, "socioIndustrial"))
+
+  events <- xml_find_all(planet, "event")
+  for(event in events) {
+    year <- as.numeric(substr(xml_text(xml_find_first(event, "date")),1,4))
+    if(year>target.year) {
+      next
+    }
+    if(!is.na(xml_find_first(event, "faction"))) {
+      faction <- xml_text(xml_find_first(event, "faction"))
+      #for now if faction is multiple just take the first
+      faction <- strsplit(faction,",")[[1]][1]
+    }
+    if(!is.na(xml_find_first(event, "hpg"))) {
+      hpg <- xml_text(xml_find_first(event, "hpg"))
+    }
+    #only take changes to default SIC within 30 years of target date
+    if(!is.na(xml_find_first(event, "socioIndustrial")) & (target.year-year)<=30) {
+      sic <- xml_text(xml_find_first(event, "socioIndustrial"))
+    }
+  }
+  
+  #do some re-coding
+  sic_codes <- c(NA,NA,NA,NA,NA)
+  if(!is.na(sic)) {
+    sic_codes <- strsplit(sic, "-")[[1]]
+  }
+  
+  temp <- c(pname, xcood, ycood, sic_codes, faction, hpg)
+  print(temp)
+  planet.table <- rbind(planet.table, temp)
+}
+
+
+planets_new <- data.frame(name=planet.table[,1],
+                          x=as.numeric(planet.table[,2]),
+                          y=as.numeric(planet.table[,3]),
+                          faction=factor(planet.table[,9]),
+                          hpg=factor(planet.table[,10], levels=c("X","A","B","C","D"),ordered=TRUE),
+                          tech=    factor(planet.table[,4], levels=c("A","B","C","D","F"), ordered = TRUE),
+                          industry=factor(planet.table[,5], levels=c("A","B","C","D","F"), ordered = TRUE),
+                          raw     =factor(planet.table[,6], levels=c("A","B","C","D","F"), ordered = TRUE),
+                          output  =factor(planet.table[,7], levels=c("A","B","C","D","F"), ordered = TRUE),
+                          agricul =factor(planet.table[,8], levels=c("A","B","C","D","F"), ordered = TRUE))
+
+planets_new$distance <- sqrt(planets_new$x^2+planets_new$y^2)
+planets_new$distance.sq <- planets_new$distance^2
+
+#TODO: Add in capitals
+
+#crude: for now lets remove planets that are more than 750 light years away from Terra
+planets_new <- subset(planets_new, distance<750 & faction!="ABN")
+
+library(mice)
+
+imp <- mice(planets_new[,-c(1:3)], 1)
+#that broke quicklly
+
+
+library(ggplot2)
+
+ggplot(planets_new, aes(x=x, y=y, color=tech))+
+  geom_point(alpha=0.5)+
+  theme_minimal()+
+  scale_colour_brewer(direction=-1)
+ggplot(cbind(planets_new[,1:3],complete(imp,1)), aes(x=x, y=y, color=tech))+
+  geom_point(alpha=0.5)+
+  theme_minimal()+
+  scale_colour_brewer(direction=-1)
+
+
+library(MASS)
+library(VGAM)
+
+model.tech <- vglm(cbind(planets_new$tech=="A", 
+                         planets_new$tech=="B",
+                         planets_new$tech=="C",
+                         planets_new$tech=="D",
+                         planets_new$tech=="F")~distance+distance.sq+faction+hpg, 
+                   data=planets_new, family=cumulative(reverse=TRUE, parallel=TRUE))
+
+x <- 0:750
+y <- -0.00291*x+0.000009276*(x)^2
+plot(x, y, type="l")
+
+
+table(planets_new$tech, planets_new$industry)
+table(complete(imp,1)$tech, complete(imp,1)$industry)
---
+++ system_creation_functions.R
@@ -0,0 +1,220 @@
+#This script contains functions for creating planets
+
+roll_d6 <- function(n) {
+  return(sum(sample(1:6, n, replace=FALSE)))
+}
+
+generate_system <- function(habitable=TRUE) {
+  
+  ##### Star Type #####
+  star_type <- c("F","M","G","K",rep("M",6),"F")
+  if(habitable) {
+    star_type <- c(rep("M",3),"K","K","G","G",rep("F",4))
+  }
+
+  spectral_class <- star_type[roll_d6(2)-1]
+  subtype <- sample(0:9,1)
+  ##A tweak here. Classes M2 or greater have no habitable zones so
+  ##only allow 0 ad 1 subtypes if that class and habitable==true
+  if(habitable & spectral_class=="M") {
+    subtype <- sample(0:1,1)
+  }
+  
+  stype <- paste(spectral_class,subtype,"V",sep="")
+  stype_data <- read.csv("data/solar_type.csv", row.names=1)[stype,]
+  
+  ##### Orbital Slots #####
+  
+  #another tweak here. B8 through B0 have a habitable zone past the 
+  #fifth position so its possible to get something out of the habitable
+  #zone on a low roll. So set a minimum value based on star type
+  minimum_slots <- 5
+  if(spectral_class == "B") {
+    if(subtype<9 & subtype>6) {
+      minimum_slots <- 6
+    } else if(subtype<=6 & subtype>2) {
+      mimimum_slots <- 7
+    } else {
+      minimum_slots <- 8
+    }
+  }
+  orbital_slots <- max(minimum_slots, roll_d6(2)+3)
+  
+  placement_constants <- c(0.4,0.7,1.0,1.6,2.8,5.2,10,19.6,
+                           38.8,77.2,154,307.6,614.8,1229.2,
+                           2458)
+  orbital_placement <- stype_data$radius*placement_constants[1:orbital_slots]
+
+  life_zone <- orbital_placement>=stype_data$distance_inner_au & 
+    orbital_placement<=stype_data$distance_outer_au
+  
+  planets <- NULL
+  terrestrial_found <- FALSE
+  
+  for(slot in 1:orbital_slots) {
+    planet <- generate_planet(orbital_placement[slot],habitable,stype_data)
+    #another tweak - if habitable is true, then we need to ensure
+    #that at least on habitable slot in the life zone is occupied by terrestrial 
+    #planet FIXME - not working but it was before
+    if(!terrestrial_found & life_zone[slot] & 
+       ((slot == orbital_slots) | !any(life_zone[(slot+1):orbital_slots]))) {
+      while(!is_habitable(planet)) {
+        planet <- generate_planet(orbital_placement[slot],habitable,stype_data)
+        terrestrial_found = TRUE
+      }
+    }
+    
+    planets <- rbind(planets, unlist(planet))
+  }
+  
+  planets <- data.frame(planets, stringsAsFactors = FALSE)
+  planets$type <- factor(planets$type)
+  planets$pressure <- factor(planets$pressure, 
+                             levels=c("Vacuum","Trace","Low","Normal","High","Very High"))
+  planets$atmosphere <- factor(planets$atmosphere)
+  planets$diameter <- as.numeric(planets$diameter)
+  planets$density <- as.numeric(planets$density)
+  planets$gravity <- as.numeric(planets$gravity)
+  planets$escape_velocity <- as.numeric(planets$escape_velocity)
+  planets$orbital_velocity <- as.numeric(planets$orbital_velocity)
+  planets$day_length <- as.numeric(planets$day_length)
+  planets$year_length <- as.numeric(planets$year_length)
+  
+  #show work for now
+  return(list(star=list(type=stype, charge=stype_data$charge),planets=planets))
+}
+
+generate_planet <- function(radius, habitable_system, system_data) {
+ 
+  life_zone <- radius>=system_data$distance_inner_au & radius<=system_data$distance_outer_au
+  outer <- radius > system_data$distance_outer_au
+  
+  #get basic type  
+  InnerType <- c(rep("Empty",2),
+                 "Asteroid Belt",
+                 "Dwarf Terrestrial",
+                 rep("Terrestrial",2),
+                 "Giant Terrestrial",
+                 rep("Gas Giant",2),
+                 rep("Ice Giant",2))
+  
+  OuterType <- c(rep("Empty",2),
+                 "Asteroid Belt",
+                 "Dwarf Terrestrial",
+                 rep("Gas Giant",3),
+                 "Terrestrial",
+                 "Giant Terrestrial",
+                 rep("Ice Giant",2))
+  
+  if(outer) {
+    type <- OuterType[roll_d6(2)-1]
+  } else {
+    type <- InnerType[roll_d6(2)-1]
+  }
+  
+  #now fill in stuff
+  diameter <- NA
+  density <- NA
+  day <- NA
+  year <- NA
+  pressure <- NA
+  atmosphere <- NA
+  
+  if(type=="Dwarf Terrestrial") {
+    diameter <- 400+100*roll_d6(3)
+    density <- roll_d6(1)
+    day <- roll_d6(3)+12
+    pressure <- "Vacuum"
+    atmosphere <- "None"
+  } else if(type=="Terrestrial") {
+    diameter <- 2500+1000*roll_d6(2)
+    density <- 2.5+roll_d6(1)^(0.75)
+    day <- roll_d6(3)+12
+  } else if(type=="Giant Terrestrial") {
+    diameter <- 12500+1000*roll_d6(1)
+    density <- 2+roll_d6(1)
+    day <- roll_d6(4)
+    pressure <- "Very High"
+    atmosphere <- "Unbreathable"
+  } else if(type=="Gas Giant") {
+    diameter <- 50000+10000*roll_d6(2)
+    density <- 0.5+roll_d6(2)/10
+    day <- roll_d6(4)
+    pressure <- "Very High"
+    atmosphere <- "Unbreathable"
+  } else if(type=="Ice Giant") {
+    diameter <- 25000+5000*roll_d6(1)
+    density <- 1+roll_d6(2)/10
+    day <- roll_d6(4)
+    pressure <- "Very High"
+    atmosphere <- "Unbreathable"
+  } else if(type=="Asteroid Belt") {
+    #TODO
+  }
+  
+  #determine atmosphere for terrestrial
+  if(type=="Terrestrial" | (type=="Giant Terrestrial" & roll_d6(1)==6)) {
+    pressure <- c(rep("Vacuum",2),"Trace",rep("Low",2),rep("Normal",2),
+                  rep("High",2),rep("Very High",2))[roll_d6(2)-1]
+    
+    if(life_zone) {
+      habitable_roll <- roll_d6(2)+system_data$habitability
+      if(pressure=="Low" | pressure=="High") {
+        habitable_roll <- habitable_roll-1
+      }
+      if(type=="Giant Terrestrial") {
+        habitable_roll <- habitable_roll-2
+      }
+      #Tweak to make habitabile planets more likely in systems that must be inhabited
+      if(habitable_system) {
+        habitable_roll <- habitable_roll+2
+      }
+      if(pressure=="Vacuum" | pressure=="Trace" | pressure=="Very High" | habitable_roll<9) {
+        atmosphere <- "Unbreathable"
+      } else {
+        atmo_roll <- roll_d6(2)
+        if(type=="Giant Terrestrial") {
+          atmo_roll <- atmo_roll-2
+        }
+        if(atmo_roll<2) {
+          atmosphere <- "Toxic"
+        } else if(atmo_roll<7) {
+          atmosphere <- "Tainted"
+        } else {
+          atmosphere <- "Breathable"
+          
+        }
+      }
+    } else {
+      atmosphere <- "Toxic"
+    }
+  }
+  
+  gravity <- (diameter/12742)*(density/5.5153)
+  escape_velocity <- 11186*(diameter/12742)*sqrt(density/5.5153)*3.6
+  orbital_velocity <- escape_velocity/sqrt(2)
+  year_length <- sqrt(radius^3+system_data$mass)
+  
+  transit_distance <- sqrt((system_data$safe_jump*1000)^2+
+                             (radius*149597871000)^2)
+  transit_time <- (2 * sqrt(transit_distance/9.8))/(24*60*60)
+  
+  return(list(type=type, orbital_dist=radius, life_zone=life_zone,
+              pressure=pressure, atmosphere=atmosphere, 
+              gravity=round(gravity,2), transit_time=round(transit_time,2),
+              diameter=diameter, density=round(density,4), 
+              escape_velocity=round(escape_velocity),
+              orbital_velocity=round(orbital_velocity), 
+              day_length=day, year_length=round(year_length,1)))
+  
+}
+
+is_habitable <- function(planet) {
+  return(!is.na(planet$atmosphere) & 
+           !is.na(planet$pressure) &
+           planet$atmosphere!="Unbreathable" & 
+           planet$atmosphere!="None" & 
+           planet$pressure!="Very High" & 
+           planet$pressure!="Vacuum" &
+           planet$pressure!="Trace")
+}
