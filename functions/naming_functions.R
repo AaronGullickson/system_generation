20820547c4e7e253a60830b8ff4638c9e387a470
Aaron Gullickson <aarong@uoregon.edu>
2019-09-10 10:29:34 -0700

    added in possibility of [Planet name] City and [Landmass] City for capital city

--- functions/naming_functions.R
+++ functions/naming_functions.R
@@ -1,8 +1,4 @@
-#What do we need to name?
-# landmasses
-# capital city
-# moons
-# planets
+#functions for random name generation
 
 library(wrswoR)
 library(here)
@@ -70,9 +66,16 @@ generate_system_names <- function(system, id=NA) {
                                                  collapse=", ")
     }
     if(!is.na(system$planets$population[i])) {
-      #some chance that you just inherit the planet name plus "City"
-      #also some chance you get "<Landmass> City"
-      system$planets$capitol_name[i] <- sample_names(1, "city", nationality)
+      if(sample(1:100,1)<=10) {
+        #some chance that you just inherit the planet name plus "City"
+        system$planets$capitol_name[i] <- paste(system$planets$name, "City")
+      } else if(sample(1:100,1)<=5 && !is.na(system$planets$continent_names[i]) && 
+                nchar(system$planets$continent_names[i])>0) {
+        system$planets$capitol_name[i] <- paste(strsplit(system$planets$continent_names[i], ",")[[1]][1], 
+                                                "City")
+      } else {
+        system$planets$capitol_name[i] <- sample_names(1, "city", nationality)
+      }
     }
   }
   
